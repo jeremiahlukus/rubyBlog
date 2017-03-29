5436386e3a3cab5c3f4edc3f6d758eef3a2711a1
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-03-29 12:40:03 -0400

    Action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentication => ':plan',
+    :authentication => ':login',
     :user_name      => ENV[ 'SENDGRID_PASSWORD' ],
     :password       => ENV[ 'SENDGRID_PASSWORD' ],
     :domain         => 'heroku.com',
