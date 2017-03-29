aefdb10778abecd26caaa233c46d2b72abd893af
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-03-29 12:33:29 -0400

    Fix ActionMail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => ':plan',
-    :user_name      => ENV[ 'SENDGRID_USERNAME' ],
+    :user_name      => ENV[ 'SENDGRID_PASSWORD' ],
     :password       => ENV[ 'SENDGRID_PASSWORD' ],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
