a79bae4a30a5852e5d8987c2d6b3cdc0e567bae9
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-03-29 15:46:17 -0400

    Can't figure out auth error on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
-    :authentication => ':plain',
+    :authentication => false,
     :user_name      => ENV[ 'SENDGRID_PASSWORD' ],
     :password       => ENV[ 'SENDGRID_PASSWORD' ],
     :domain         => 'heroku.com',
