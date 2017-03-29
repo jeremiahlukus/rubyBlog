556c0a1f2e8dfc70c93991d4458ef187f51294c2
Jeremiah Parrack <jeremiahlukus1@gmail.com>
2017-03-29 01:29:00 -0400

    Added edit and destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,7 +11,7 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new project_params
- 
+
     if @project.save
       redirect_to @project, notice: "Nice, keep them projects coming!"
     else
@@ -20,6 +20,22 @@ class ProjectsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @project.update project_params
+      redirect_to @project, notice: "Your project has been updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
   end
 
   private
